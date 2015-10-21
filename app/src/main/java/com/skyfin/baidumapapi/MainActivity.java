3fd7af086e9f026d2e4b14106d49479569f142c4
Skyfin <1015283564@qq.com>
2015-10-21 21:54:04 +0800

    完成百度地图环境的搭建，配置好百度地图的环境，完成了hello 百度地图

--- app/src/main/java/com/skyfin/baidumapapi/MainActivity.java
+++ app/src/main/java/com/skyfin/baidumapapi/MainActivity.java
@@ -1,13 +1,35 @@
 package com.skyfin.baidumapapi;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import com.baidu.mapapi.SDKInitializer;
 
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private TextView mHelloTvw ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //初始化百度sdk
+        SDKInitializer.initialize(getApplicationContext());
         setContentView(R.layout.activity_main);
+        mHelloTvw = (TextView) findViewById(R.id.hello_tvw);
+        mHelloTvw.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+        Intent intent  = new Intent();
+        switch (id){
+            case R.id.hello_tvw:
+                intent.setClass(getApplicationContext(),HelloBaiduMapActivity.class);
+                startActivity(intent);
+                break;
+        }
     }
 }
